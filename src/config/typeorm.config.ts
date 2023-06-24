b5af9d43de6051ea70cb81b8642d872c30d14a10
OverFlowBIN <87470206+OverFlowBIN@users.noreply.github.com>
2023-06-24 18:01:15 +0900

    Change postgres username

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -5,7 +5,7 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 export const typeORMConfig: TypeOrmModuleOptions = {
   type: 'postgres',
   host: '127.0.0.1',
-  username: 'postgres',
+  username: 'root',
   password: 'dudqls12',
   database: 'issue',
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
